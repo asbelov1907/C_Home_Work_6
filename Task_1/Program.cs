7648437c3ca9cd082db2ed0d001721a4392246e1
AntonBelov <asbelov1907@gmail.com>
2022-05-27 02:06:26 +0300

    Задание 1 выполнено

---
+++ Task_1/Program.cs
@@ -0,0 +1,34 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+int[] InputNumbers()
+{
+    Console.WriteLine("Сколько планируете ввести чисел?");
+    int SizeNumbers = int.Parse(Console.ReadLine());
+
+    int[] Array = new int[SizeNumbers];
+    Console.WriteLine("Введите " + SizeNumbers + " чисел/ла. После каждого введенного числа нажимайте Enter.");
+    for (int i = 0; i < SizeNumbers; i++)
+    {
+        Array[i] = int.Parse(Console.ReadLine());
+
+    }
+    return Array;
+
+}
+
+void result(int[] arr)
+{
+    int res = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0)
+            res++;
+    }
+    Console.WriteLine("Чисел больше 0: " + res);
+}
+
+int[] arr = InputNumbers();
+result(arr);
+
